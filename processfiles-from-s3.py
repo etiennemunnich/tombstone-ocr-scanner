cdb620ea2790344f4a351e353439bb9f85aa9c9e
Etienne Munnich <etienne.munnich@gmail.com>
2017-12-28 14:31:05 +0200

    update
    - stores json output file in the s3 bucket
    - checks if json output file exists, if so doesn't process the file

--- processfiles-from-s3.py
+++ processfiles-from-s3.py
@@ -10,14 +10,15 @@ import json
 import sys
 
 # PHOTOS Directory/bucket
-photo_dir = 'folderwherefilesarestoredlocally/'
-photo_bucket = 'mybucket'
+photo_dir = '/yourlocalfiles/photos/'
+photo_bucket = 'yourbuckethere'
 photo_bucket_prefix = ''
 
 # Get files (folder)
-files = glob.glob(photo_dir + "*.jpg")
-files.extend(glob.glob(photo_dir + "*.JPG"))
-print(files)
+# files = ''
+# files = glob.glob(photo_dir + "*.jpg")
+# files.extend(glob.glob(photo_dir + "*.JPG"))
+# print('Files :'files)
 
 # Get files (S3)
 s3 = boto3.resource('s3')
@@ -48,25 +49,43 @@ except: # catch *all* exceptions
 for idx, value in enumerate(s3jpgobjectnames):
     print("Processing :" + s3jpgobjectnames[idx])
     print("Value :" + value)
-    rek_response = myrekognition.detect_text(
-        Image={
-            'S3Object': {
-                'Bucket': photo_bucket,
-                'Name': s3jpgobjectnames[idx]
-                }
-        }
-    )
-    newfilename = photo_dir + s3jpgobjectnames[idx] + ".json"
-    print("New object files name: " + newfilename)
-    try:
-        with open(newfilename, 'wb') as newfilejson:
-            filedata = json.dumps(rek_response, sort_keys=False, indent=2).__str__()
 
-            newfilejson.write(filedata)
-            print(filedata)
-            #newfilejson.close()   Apparrently not required when ising with open.
-    except: # catch *all* exceptions
-       e = sys.exc_info()[0]
-       print( "File write Error: %s" % e)
+    #check if json exists
+    if (s3jpgobjectnames[idx].rstrip('.jpg') + ".json") in s3jsonobjectnames:
+        print('Skipping as json exists')
+    else:
+        try:
+            rek_response = myrekognition.detect_text(
+                Image={
+                    'S3Object': {
+                        'Bucket': photo_bucket,
+                        'Name': s3jpgobjectnames[idx]
+                        }
+                    }
+                )
+            print(json.dumps(rek_response, sort_keys=False, indent=2).__str__())
+        except:  # catch *all* exceptions
+            e4 = sys.exc_info()[0]
+            print("AWS Rekognition submit Error: %s" % e4)
 
+        #write to s3
+        s3newfilename = photo_bucket_prefix + s3jpgobjectnames[idx].rstrip('.jpg') + ".json"
+        print('S3 New Object file name :' + s3newfilename)
+        try:
+            s3.Object(photo_bucket, s3newfilename).put(Body=json.dumps(rek_response, sort_keys=False, indent=2).__str__(), ServerSideEncryption='AES256')
+        except:  # catch *all* exceptions
+            e3 = sys.exc_info()[0]
+            print("S3 write Error: %s" % e3)
 
+        # #write file locally
+        # newfilename = photo_dir + s3jpgobjectnames[idx].rstrip('.jpg') + ".json"
+        # print("New object files name: " + newfilename)
+        # try:
+        #     with open(newfilename, 'wb') as newfilejson:
+        #         filedata = json.dumps(rek_response, sort_keys=False, indent=2).__str__()
+        #         newfilejson.write(filedata)
+        #         print(filedata)
+        #         #newfilejson.close()   Apparrently not required when ising with open.
+        # except: # catch *all* exceptions
+        #    e = sys.exc_info()[0]
+        #    print("File write Error: %s" % e)
